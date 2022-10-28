29a1063a3a5992d9a4c85a9a62fde47c3f32a0e3
Aniket Singh <singani@hamiltonstudents.org>
2022-10-28 17:40:22 -0500

    First Draft of Auto and MeepMeep
    BlueAuto1 Draft and MeepMeep Application installed

---
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepTesting.java
@@ -0,0 +1,54 @@
+package com.example.meepmeep;
+
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+
+public class MeepTesting {
+    public static void main(String args[]) {
+        MeepMeep meep = new MeepMeep(800);
+
+        RoadRunnerBotEntity ArcFlashBot = new DefaultBotBuilder(meep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 12)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(38, 60, Math.toRadians(-90) ))
+                                .strafeRight(26)
+                                .forward(50)
+                                .strafeLeft(42)
+                                .turn(Math.toRadians(90))
+                                .turn(Math.toRadians(-33.8))
+                                .strafeLeft(10)
+                                .turn(Math.toRadians(33.8))
+                                .strafeRight(8)
+                                .back(6)
+                                .turn(Math.toRadians(-33.8))
+                                .strafeLeft(10)
+                                .turn(Math.toRadians(33.8))
+                                .strafeRight(8)
+                                .back(6)
+                                .turn(Math.toRadians(-33.8))
+                                .strafeLeft(10)
+                                .turn(Math.toRadians(33.8))
+                                .strafeRight(8)
+                                .turn(Math.toRadians(90))
+                                .forward(45)
+                                .build()
+                );
+
+                meep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                .setTheme(new ColorSchemeRedDark())
+                .setBackgroundAlpha(1f)
+                .setDarkMode(true)
+                .addEntity(ArcFlashBot)
+                        .start();
+
+
+
+
+
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Blue1Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Blue1Auto.java
@@ -2,10 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
+@Autonomous(group = "drive")
 public class Blue1Auto extends LinearOpMode {
     @Override
     public void runOpMode(){
@@ -16,7 +19,52 @@ public class Blue1Auto extends LinearOpMode {
         Trajectory strafeRight = drivetrain.trajectoryBuilder(new Pose2d(0,0,0))
                 .strafeRight(26)
                 .build();
-        //drivetrain.followTrajectory(strafeRight);
+
+        Trajectory forward = drivetrain.trajectoryBuilder(strafeRight.end())
+                .forward(50)
+                .build();
+
+        TrajectorySequence strafeleft = drivetrain.trajectorySequenceBuilder(forward.end())
+                .strafeLeft(42)
+                .turn(Math.toRadians(90))
+                .build();
+
+   TrajectorySequence oblique1 = drivetrain.trajectorySequenceBuilder(strafeleft.end())
+                .turn(Math.toRadians(-33.8))
+                .strafeLeft(10)
+                .turn(Math.toRadians(33.8))
+                .strafeRight(10)
+                .back(3)
+                .turn(Math.toRadians(-33.8))
+                .strafeLeft(8)
+                .back(3)
+                .turn(Math.toRadians(33.8))
+                .strafeRight(10)
+                .build();
+
+//        TrajectorySequence oblique2 = drivetrain.trajectorySequenceBuilder(oblique1.end())
+//                .turn(Math.toRadians(-33.8))
+//                .strafeRight(5)
+//                .build();
+
+
+
+
+
+
+
+
+
+
+
+        drivetrain.followTrajectory(strafeRight);
+        drivetrain.followTrajectory(forward);
+        drivetrain.followTrajectorySequence(strafeleft);
+        drivetrain.followTrajectorySequence(oblique1);
+
+//
+
+
 
 
 
