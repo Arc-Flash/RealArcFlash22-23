3f39df434b785fc85861d497b077bb22da09341f
DCRockets <kvdtfacchini@gmail.com>
2022-10-29 16:19:59 -0500

    Made a sane auto

--- MeepMeep/src/main/java/com/example/meepmeep/MeepTesting.java
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeep;
 
 import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
@@ -14,44 +15,46 @@ public class MeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 12)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(38, 60, Math.toRadians(-90) ))
+                        drive.trajectorySequenceBuilder(new Pose2d(-38, -60, Math.toRadians(90) ))
                                 .strafeRight(26)
+                                .forward(48)
+                                .strafeLeft(43)
+                                .turn(Math.toRadians(90))
+
+                                .turn(Math.toRadians(-60))
+                                .back(5)
+                                .forward(5)
+                                .turn(Math.toRadians(60))
+
+                                .turn(Math.toRadians(-60))
+                                .back(5)
+                                .forward(5)
+                                .turn(Math.toRadians(60))
+
+                                .turn(Math.toRadians(-60))
+                                .back(5)
+                                .forward(5)
+                                .turn(Math.toRadians(60))
+
+                                .turn(Math.toRadians(-60))
+                                .back(5)
+                                .forward(5)
+                                .turn(Math.toRadians(60))
+
+                                .turn(Math.toRadians(-60))
+                                .back(5)
+                                .forward(5)
+                                .turn(Math.toRadians(60))
+
+                                .forward(4)
+                                .turn(Math.toRadians(90))
                                 .forward(50)
-                                .strafeLeft(42)
-                                .turn(Math.toRadians(90))
-                                .turn(Math.toRadians(-33.8))
-                                .strafeLeft(10)
-                                .turn(Math.toRadians(33.8))
-                                .strafeRight(8)
-                                .back(6)
-                                .turn(Math.toRadians(-33.8))
-                                .strafeLeft(10)
-                                .turn(Math.toRadians(33.8))
-                                .strafeRight(8)
-                                .back(6)
-                                .turn(Math.toRadians(-33.8))
-                                .strafeLeft(10)
-                                .turn(Math.toRadians(33.8))
-                                .strafeRight(8)
-                                .back(6)
-                                .turn(Math.toRadians(-33.8))
-                                .strafeLeft(10)
-                                .turn(Math.toRadians(33.8))
-                                .strafeRight(8)
-                                .back(6)
-                                .turn(Math.toRadians(-33.8))
-                                .strafeLeft(10)
-                                .turn(Math.toRadians(33.8))
-                                .strafeRight(8)
-                                .turn(Math.toRadians(90))
-                                .forward(45)
-                                .turn(Math.toRadians(45))
-                                .back(3)
+
                                 .build()
                 );
 
                 meep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
-                .setTheme(new ColorSchemeRedDark())
+                .setTheme(new ColorSchemeBlueDark())
                 .setBackgroundAlpha(1f)
                 .setDarkMode(true)
                 .addEntity(ArcFlashBot)
